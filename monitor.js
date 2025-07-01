004b84fc9e58b2a330e055d5cc66d707d317a72b
Cam <cam00clark@gmail.com>
2025-07-01 17:54:59 +1200

    created a object passed to array, need to complete inStock() and discordNotification()

--- monitor.js
+++ monitor.js
@@ -1,25 +1,57 @@
 
-async function check() {
+var productsLength;
+var currentProducts = [];
+
+async function grabProducts() {
 	const products =  "https://blonded.co/products.json";
 
-		const response = await fetch(products, );
+		const response = await fetch(products);
 		
 		if(!response.ok){
 		throw new error(`Response Status: ${response.status}`);
 	} 
 
 	const json = await response.json();
-	productsArray = json.products;
+	var productsArray = json.products;
 	
-	console.log(productsArray[0].variants);
-
+	checkNewProdcuts(productsArray);
+	checkIfInStock(productsArray);
 	
-
-
-	// }catch(error){
-	// 	console.log(" penis");		
-	// }
-
 }
 
-check();
+
+function checkNewProdcuts(productsArray){
+	// console.log(productsArray)
+	var currentProducts  = [] ;
+	
+
+	for(var i = 0; i <  productsArray.length; i++){
+		var curr = productsArray[i].variants[0];
+		var title = productsArray[i].title;
+		// console.log(title);
+		// console.log(curr);
+		const product = {
+			product_id : curr.featured_image.product_id,
+			product_name : title,
+			available: curr.available,
+			img : curr.featured_image.src,
+		}
+		if(!currentProducts.includes(product)){
+			// discordNotification(product)
+			currentProducts.push(product);
+			console.log(product);
+		}
+
+	}
+}
+
+function checkIfInStock(productsArray){
+	
+}
+
+
+// discordNotification(product){
+
+// }
+
+grabProducts();
